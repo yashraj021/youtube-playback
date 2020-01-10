4fe8acf215a4ec6a63aa6d51ab375dc8b2e10861
str1k3r-g0d <yash.raj021@gmail.com>
2020-01-10 20:14:36 +0530

    Updates

---
+++ precache-manifest.eaf6df76499d0f9a463422e1ef6ff6cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed855193cafc59d7a7cabe15163671a8",
+    "url": "/youtube-playback/index.html"
+  },
+  {
+    "revision": "7eeebd98e788b61d2dab",
+    "url": "/youtube-playback/static/css/main.406d83a9.chunk.css"
+  },
+  {
+    "revision": "105a766b0ffaf26ac57d",
+    "url": "/youtube-playback/static/js/2.8e6f6bcc.chunk.js"
+  },
+  {
+    "revision": "2e4c155d0ebd06fdb333036ecc18d462",
+    "url": "/youtube-playback/static/js/2.8e6f6bcc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7eeebd98e788b61d2dab",
+    "url": "/youtube-playback/static/js/main.3d3a2a64.chunk.js"
+  },
+  {
+    "revision": "ea04c357fae890ea99a2",
+    "url": "/youtube-playback/static/js/runtime-main.dd739759.js"
+  }
+]);
